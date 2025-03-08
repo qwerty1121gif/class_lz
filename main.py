6ffac90f05bdd1f221b42573ed28dc81da41070d
qwerty1121gif <Vladimirstalin@tutamail.com>
2025-03-09 00:41:42 +0300

    ф

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ print("2. Вписанная окружность:")
 print(f"   Радиус: {octagon.inscribed_circle_radius():.2f}")
 print(f"   Площадь: {octagon.inscribed_circle_area():.2f}\n")
 
-print("3. Параметры октагона:")
+print("3. Параметры восьмиугольника:")
 print(f"   Площадь: {octagon.octagon_area():.2f}")
 print(f"   Периметр: {octagon.octagon_perimeter()}\n")
 
